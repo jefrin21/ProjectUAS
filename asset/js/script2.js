e00fe0beeadc06eb4e51f7ce317b665f3a7e49ce
uwdwd <cenalvin000@gmail.com>
2023-12-09 11:14:17 +0700

    hello

--- asset/js/script2.js
+++ asset/js/script2.js
@@ -20,3 +20,30 @@ function handleMouseMove(e) {
 function resetZoom() {
   zoomedImage.style.transform = "scale(1)";
 }
+
+var spanText = function spanText(text) {
+  var string = text.innerText;
+  var spaned = "";
+  for (var i = 0; i < string.length; i++) {
+    if (string.substring(i, i + 1) === " ")
+      spaned += string.substring(i, i + 1);
+    else spaned += "<span>" + string.substring(i, i + 1) + "</span>";
+  }
+  text.innerHTML = spaned;
+};
+
+var headline = document.querySelector(".rekomendasi");
+
+spanText(headline);
+
+let letters = headline.textContent.split("  ");
+headline.textContent = "";
+
+letters.forEach((letter, i) => {
+  let span = document.createElement("span");
+  span.textContent = letter;
+  span.style.animation = `slideLeft 1.5s forwards ${
+    i * 0.1
+  }s cubic-bezier(0.075, 0.82, 0.165, 1)`;
+  headline.appendChild(span);
+});
